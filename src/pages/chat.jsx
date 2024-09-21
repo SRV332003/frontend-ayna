7f9b307019ea80b6c637611713d5809f1cbb93e2
Dhruv Mehta <dhruvmehta382@gmail.com>
2024-09-21 13:10:49 +0530

    feat : frnend to backend connection added

--- src/pages/chat.jsx
+++ src/pages/chat.jsx
@@ -2,17 +2,17 @@ import { useEffect, useState } from "react";
 import { PrimaryButton } from "../components/Button";
 import LeftMsg from "../components/leftMsg";
 import RightMsg from "../components/rightMsg";
-import socketio from "socket.io-client";
+// import  from "socket.io-client";
 
 
 
 const Chat = () => {
     const username = localStorage.getItem("User");
-    
-    const [username2,setuser2] = localStorage.getItem("");
+    const [loader,setloader] = useState(true);
+    const [user2,setuser2] = useState("");
     const [messages, setMessages] = useState([]);
-    const [conn, setConn] = useState(null);
-    
+    const [conn, setConn] = useState(null)
+
     const handleSend = async (e) => {
         e.preventDefault();
         let jsonData = Object.fromEntries(new FormData(e.target).entries());
@@ -35,7 +35,7 @@ const Chat = () => {
             }
         }
 
-        conn.emit("message", JSON.stringify(event));
+        conn.send("message", JSON.stringify(event));
 
         updateMessages(msg);
         e.target.reset();
@@ -43,18 +43,22 @@ const Chat = () => {
 
     useEffect(() => {
         localStorage.setItem("messages_"+username, JSON.stringify(messages));
-        document.getElementById("chatbox").scrollTop = document.getElementById("chatbox").scrollHeight;
+        //document.getElementById("chatbox").scrollTop = document.getElementById("chatbox")?.scrollHeight;
         console.log(messages);
-        conn && conn.on("message",(e) => {
+        
+        conn && conn.addEventListener("message",(e) => {
             console.log(e);
-            const event = JSON.parse(e);
+            const event = JSON.parse(e.data);
             if(event.type === "message"){
                 const data = event.data;
                 updateMessages(data);
             }
-            if(event.type === "init"){
+            if(event.type === "INIT"){
                 const data = event.data;
-                setuser2(data);
+                setloader(false);
+                console.log(data);
+                
+                setuser2(user2);
             }
         });
     }, [messages])
@@ -66,50 +70,60 @@ const Chat = () => {
 
     useEffect(() => {
         if (conn == null) {
-            setConn(new socketio.connect("wss://strapi-backend-ayna.onrender.com/", {
-                path: "/chatws"
-            }));
-        }else
-        conn.on("message",(e) => {
-            console.log(e);
-            const event = JSON.parse(e);
-            if(event.type === "message"){
-                const data = event.data;
-                updateMessages(data);
-            }
-        });
+            setConn(new WebSocket("ws://localhost:8080/ws"));
+        }else{
+            conn.addEventListener("message", (e) => {
+                console.log(e);
+                const event = JSON.parse(e.data);
+                if(event.type === "message"){
+                    const data = event.data;
+                    updateMessages(data);
+                }
+                if(event.type === "INIT"){
+                    const u = event.user;
+                    setloader(false);
+                    console.log(u);
+                    
+                    setuser2(u);
+                }
+            })
+        }
     }, [conn])
 
     
 
     return (
-        <div className="border p-3 bg-white rounded-lg m-4 md:m-auto md:w-1/3">
-            <h1 className="text-2xl font-bold text-center border-b ">Server Chat</h1>
-            <div className="flex flex-row gap-2 ">
-                <div className="w-full">
-                    <div className="overflow-auto scroll-smooth" id="chatbox" style={{
-                        height: "calc(100vh - 11rem)"
-                    }}>
-                        <div className="flex flex-col gap-2 p-2 w-full">
-                            {messages && messages.map((msg, index) => {
-                                if (msg.identifier === username) {
-                                    return <RightMsg key={index} message={msg.message} time={msg.time} />
-                                } else {
-                                    return <LeftMsg key={index} message={msg.message} time={msg.time} />
-                                }
-                            })}
+        loader ?  (
+            <div>Loading ....</div>
+        ) : (
+            <div className="border p-3 bg-white rounded-lg m-4 md:m-auto md:w-1/3">
+                <h1 className="text-2xl font-bold text-center border-b ">Server Chat  with {user2}</h1>
+                <div className="flex flex-row gap-2 ">
+                    <div className="w-full">
+                        <div className="overflow-auto scroll-smooth" id="chatbox" style={{
+                            height: "calc(100vh - 11rem)"
+                        }}>
+                            <div className="flex flex-col gap-2 p-2 w-full">
+                                {messages && messages.map((msg, index) => {
+                                    if (msg.identifier === username) {
+                                        return <RightMsg key={index} message={msg.message} time={msg.time} />
+                                    } else {
+                                        return <LeftMsg key={index} message={msg.message} time={msg.time} />
+                                    }
+                                })}
+                            </div>
                         </div>
+                        <form className="flex flex-row w-full overflow-auto" onSubmit={handleSend}
+                        >
+                            <input name="message" type="text" className="p-2 border w-full rounded-md" placeholder="Send Something..." />
+                            <button type="submit" >
+                                <PrimaryButton className=" p-2  text-white ">Send</PrimaryButton>
+                            </button>
+                        </form>
                     </div>
-                    <form className="flex flex-row w-full overflow-auto" onSubmit={handleSend}
-                    >
-                        <input name="message" type="text" className="p-2 border w-full rounded-md" placeholder="Send Something..." />
-                        <button type="submit" >
-                            <PrimaryButton className=" p-2  text-white ">Send</PrimaryButton>
-                        </button>
-                    </form>
                 </div>
             </div>
-        </div>
+        )
     );
 }
 
